61fcff879caea2fc2d3f747ad9859053aa8cd47e
raluca.toth@8x8.com <raluca.toth@8x8.com>
2022-01-26 14:23:38 +0200

    Add a basic web socket server

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,45 @@
 import express from 'express';
 import http from 'http';
+import Redis from 'ioredis';
 
 import config from './config/config';
 import SessionsHandler from './handlers/sessions_handler';
 import RestServer from './rest_server';
 import SessionsService from './service/sessions_service';
+import { generateNewContext } from './util/context';
 import logger from './util/logger';
+import WsServer from './ws_server';
 
 logger.info('Starting up jitsi-component-selector service with config', {
     config
 });
 
-// configure the server
+// configure Redis
+const redisOptions = <Redis.RedisOptions>{
+    host: config.RedisHost,
+    port: config.RedisPort
+};
+
+if (config.RedisPassword) {
+    redisOptions.password = config.RedisPassword;
+}
+
+if (config.RedisTLS) {
+    redisOptions.tls = {};
+}
+
+if (config.RedisDb) {
+    redisOptions.db = config.RedisDb;
+}
+
+const pubClient = new Redis(redisOptions);
+const subClient = pubClient.duplicate();
+
+// configure the http server
 const app = express();
 const httpServer = http.createServer(app);
 
+// configure the rest server
 const sessionsService = new SessionsService();
 const restServer = new RestServer({
     app,
@@ -25,6 +50,19 @@ const restServer = new RestServer({
 });
 
 restServer.init();
+
+// configure the web socket server
+const websocketServer = new WsServer({
+    httpServer,
+    pubClient,
+    subClient,
+    wsPath: config.WSServerPath
+});
+
+const initCtx = generateNewContext();
+
+websocketServer.init(initCtx);
+
 
 // start server
 
---
+++ src/ws_server.ts
@@ -0,0 +1,76 @@
+import { createAdapter } from '@socket.io/redis-adapter';
+import * as http from 'http';
+import { Redis } from 'ioredis';
+import { Server, Socket } from 'socket.io';
+
+import { Context } from './util/context';
+
+export interface WsServerOptions {
+    httpServer: http.Server;
+    pubClient: Redis;
+    subClient: Redis;
+    wsPath: string;
+}
+
+interface QueryObject {
+    instanceKey: string;
+}
+
+/**
+ * Configures the web socket server behavior
+ */
+export default class WsServer {
+    private readonly io: Server;
+
+    /**
+     * Constructor
+     * @param options WsServerOptions
+     */
+    constructor(options: WsServerOptions) {
+        this.io = new Server(options.httpServer, { path: options.wsPath });
+        this.io.adapter(createAdapter(options.pubClient, options.subClient));
+    }
+
+    /**
+     * Initializes middleware and routes
+     * @param ctx
+     */
+    public init(ctx: Context): void {
+        // stats.hookToServer(this.io);
+        this.configRoutes(ctx);
+    }
+
+    /**
+     * Configures websocket routes and behavior
+     * @param ctx Context
+     * @private
+     */
+    private configRoutes(ctx: Context): void {
+        this.io.on('connection', async (socket: Socket) => {
+            ctx.logger.info(`[ws][${socket.id}] Client connected`);
+            const queryObject = socket.handshake.query as unknown as QueryObject;
+
+            if (!queryObject.instanceKey || queryObject.instanceKey.length === 0) {
+                ctx.logger.error(`[ws][${socket.id}] Client connected without an instanceKey, disconnecting it`);
+                socket.disconnect(true);
+            } else {
+                ctx.logger.info(`[ws][${socket.id}] Joining room ${queryObject.instanceKey}`);
+                socket.join(queryObject.instanceKey);
+
+                socket.on('status-updates', (report: any) => {
+                    ctx.logger.info(`[ws][${socket.id}] Got status updates from client. `, {
+                        report
+                    });
+                });
+
+                socket.on('disconnecting', () => {
+                    ctx.logger.info(`[ws][${socket.id}] Disconnecting client from rooms`, { rooms: socket.rooms });
+                });
+
+                socket.on('disconnect', () => {
+                    ctx.logger.info(`[ws][${socket.id}] Client disconnected`);
+                });
+            }
+        });
+    }
+}
